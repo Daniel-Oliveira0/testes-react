8185e1952be9de0900775a4621ef20b2e54de7e5
dnvinicius00@gmail.com <dnvinicius00@gmail.com>
2025-03-07 14:34:01 -0300

    criando um component teste

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Button from './components/button/button';
+function App() {
+  return (
+    <>
+    <Button/><Button/><Button/><Button/><Button/><Button/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/button/button.js
@@ -0,0 +1,9 @@
+import './button.css';
+
+const Button = () => {
+    return (
+        <button className='btn'>Click me</button>
+    );
+}
+
+export default Button;
